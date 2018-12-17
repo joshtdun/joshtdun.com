7a412e35dff43bad570717bd9253daf04e754f3e
joshtdun <josh@joshtdun.com>
2018-12-17 09:37:30 -0500

    added models from the blog and started the framework for user authentication

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    message = models.TextField(max_length=5000)
+    topics = models.ManyToManyField(Topic)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='blogPosts', on_delete='cascade')
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete='cascade')
+
+
+class Comment(models.Model):
+    message = models.TextField(max_length=4000)
+    blogPost = models.ForeignKey(Topic, related_name='comment', on_delete='cascade')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='comment', on_delete='cascade')
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete='cascade')    
