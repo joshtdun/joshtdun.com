78cdc715a9877cb042f30af0794236842157c002
Josh Dunsing <josh@joshtdun.com>
2019-01-26 22:43:58 +0000

    added markdon with editor and fixed the CSSon toolbar

--- joshtdun/settings.py
+++ joshtdun/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '00000000000000000000000000000000000000000000000' 
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -84,10 +84,9 @@ WSGI_APPLICATION = 'joshtdun.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE':  'django.db.backends.sqlite3',
-        'NAME':    'database.db',
-    },
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 PASSWORD_HASHERS = [
