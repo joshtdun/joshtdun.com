f67a796ce008c72330b5b93512c18b9d4cb67794
Josh Dunsing <josh@joshtdun.com>
2019-01-30 01:08:45 +0000

    redesign part 1

--- blog/tests/test_templatetags.py
+++ blog/tests/test_templatetags.py
@@ -1,3 +1,8 @@
+'''
+Tests from https://github.com/sibtc/django-beginners-guide/tree/master/boards/tests
+Check out https://simpleisbetterthancomplex.com/series/beginners-guide/1.11/
+A Great Extremely Thorough Tutorial!
+'''
 from django import forms
 from django.test import TestCase
 from blog.templatetags.form_tags import field_type, input_class
